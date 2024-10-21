6d003c5f7db441045bd1dc5e39fcd77b54642f75
burmist-git <burm.leonid@gmail.com>
2024-10-21 16:12:40 +0200

    add test_test.sh run with singularity cscs

--- test_test.sh
+++ test_test.sh
@@ -5,6 +5,7 @@ function printHelp {
     echo " --> ERROR in input arguments "
     echo " [0] --t01    : test01"
     echo " [0] --t02    : test02"
+    echo " [0] --t03    : test03 (singularity cscs)"
     echo " [0] -h       : print help"
 }
 
@@ -36,6 +37,21 @@ else
 			--write-parameters \
 			--no-write-showers \
 			--DataWriter.write_r1_waveforms=True
+    elif [ "$1" = "--t03" ]; then
+	input="/scratch/snx3000/lburmist/simtel_data/gamma_st/data/corsika_run1.simtel.gz"
+	output="/scratch/snx3000/lburmist/ctapipe_data/gamma_st/data/gamma_run1.r1.dl1.h5"
+	config="./configs/ctapipe_standard_sipm_config.json"
+	siffile=/scratch/snx3000/lburmist/singularity/21.10.2024/ctapipe.sif
+	singularity run -B /scratch/snx3000/lburmist/:/scratch/snx3000/lburmist/ $siffile \
+		    ctapipe-process --overwrite \
+		    --input=$input \
+		    --output=$output \
+		    --config=$config \
+		    --max-events=10 \
+		    --write-images \
+		    --write-parameters \
+		    --no-write-showers \
+		    --DataWriter.write_r1_waveforms=True
     elif [ "$1" = "-h" ]; then
         printHelp
     else
